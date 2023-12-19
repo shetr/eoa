0c0beab4130f0bb22a17e4f315ecdddd410f5dbf
Petr Šádek <shetr.git@gmail.com>
2023-12-19 16:45:25 +0100

    gtsp representation in progress

--- src/gtsp.rs
+++ src/gtsp.rs
@@ -1,2 +1,25 @@
+use std::rc::Rc;
+
 use crate::*;
 
+pub struct GtspProblem {
+    pub vert_count: usize,
+    pub groups: Vec<usize>,
+    pub distances: DistanceHalfMatrix
+}
+
+#[derive(Clone)]
+pub struct GtspPermutation {
+    pub spec: Rc<GtspProblem>,
+    pub groups_perm: Vec<usize>,
+    pub group_vert_indices: Vec<usize>
+}
+
+impl OptData for GtspPermutation {
+    fn dim(&self) -> usize {
+        self.spec.vert_count
+    }
+}
+
+pub struct GtspFitness {
+}
